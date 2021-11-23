48d270ad8f95e0da3fcd8ad0e0b71a0454ce65df
andreidra <andojtein@icloud.com>
2021-11-23 12:21:20 +0100

    implemented firebase utils, included ability to store authentticated user in our database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+
 import { signInWithGoogle } from "../../firebase/firebase.utils";
+
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -21,7 +24,7 @@ class SignIn extends React.Component {
   };
 
   handleChange = (event) => {
-    const { value, name } = event.targer;
+    const { value, name } = event.target;
 
     this.setState({ [name]: value });
   };
@@ -36,25 +39,23 @@ class SignIn extends React.Component {
           <FormInput
             name="email"
             type="email"
-            handleChange={this.handleSubmit}
+            handleChange={this.handleChange}
             value={this.state.email}
             label="email"
             required
           />
-
           <FormInput
             name="password"
             type="password"
             value={this.state.password}
-            handleChange={this.handleSubmit}
+            handleChange={this.handleChange}
             label="password"
             required
           />
           <div className="buttons">
-            <CustomButton type="submit"> Sign In </CustomButton>
+            <CustomButton type="submit"> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              {" "}
-              Sign in with Google{" "}
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
